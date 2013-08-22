d433fe3f6b22df9488e6ad8c3030338f2e6c08a7
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2013-08-22 11:11:43 -0400

    updated example to use port 3000 for rails s

--- examples/invite_users.rb
+++ examples/invite_users.rb
@@ -3,7 +3,7 @@ require 'discourse_api'
 # $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 # require File.expand_path('../../lib/discourse_api', __FILE__)
 
-client = DiscourseApi::Client.new("l.discourse")
+client = DiscourseApi::Client.new("localhost",3000)
 client.api_key = "YOUR_API_KEY"
 client.api_username = "YOUR_USERNAME"
 
