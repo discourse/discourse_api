e23bb288e90a00f23879f51a7cf1e20347a5091a
Michael Herold <michael.j.herold@gmail.com>
2013-11-06 11:26:33 -0500

    Remove now-redundant code.

--- lib/discourse_api.rb
+++ lib/discourse_api.rb
@@ -1,8 +1,5 @@
 require "discourse_api/version"
-require "discourse_api/parsed_path"
-require "discourse_api/resource"
 require "discourse_api/client"
-require "discourse_api/api"
 require "set"
 
 module DiscourseApi
