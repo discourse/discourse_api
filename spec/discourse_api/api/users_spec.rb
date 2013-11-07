3390f165da548327dcee30194b306538cecf1fe0
Michael Herold <michael.j.herold@gmail.com>
2013-11-06 22:05:42 -0500

    Add test for API::Users.user and stub tests for rest of module.

---
+++ spec/discourse_api/api/users_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe DiscourseApi::API::Users do
+  subject { DiscourseApi::Client.new("http://localhost") }
+
+  describe "#toggle_avatar" do
+    it "needs to have a test written for it"
+  end
+
+  describe "#user" do
+    before do
+      stub_get("http://localhost/user/test_user.json").to_return(body: fixture("user.json"), headers: { content_type: "application/json" })
+    end
+
+    it "requests the correct resource" do
+      subject.user("test_user")
+      expect(a_get("http://localhost/user/test_user.json")).to have_been_made
+    end
+
+    it "returns the requested user" do
+      user = subject.user("test_user")
+      expect(user).to be_a Hash
+    end
+  end
+
+  describe "#update_avatar" do
+    it "needs to have a test written for it"
+  end
+
+  describe "#update_email" do
+    it "needs to have a test written for it"
+  end
+
+  describe "#update_user" do
+    it "needs to have a test written for it"
+  end
+
+  describe "#update_username" do
+    it "needs to have a test written for it"
+  end
+end
