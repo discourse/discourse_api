627f902c6e26d0fd4c48af4eabbf793c1c77f267
Leonard Teo <leonard@ballistiq.com>
2014-02-04 14:44:41 -0500

    Fixed issue with user resource returning 404 error. Should get from /users/

--- spec/discourse_api/api/users_spec.rb
+++ spec/discourse_api/api/users_spec.rb
@@ -9,12 +9,12 @@ describe DiscourseApi::API::Users do
 
   describe "#user" do
     before do
-      stub_get("http://localhost/user/test_user.json").to_return(body: fixture("user.json"), headers: { content_type: "application/json" })
+      stub_get("http://localhost/users/test_user.json").to_return(body: fixture("user.json"), headers: { content_type: "application/json" })
     end
 
     it "requests the correct resource" do
       subject.user("test_user")
-      expect(a_get("http://localhost/user/test_user.json")).to have_been_made
+      expect(a_get("http://localhost/users/test_user.json")).to have_been_made
     end
 
     it "returns the requested user" do
