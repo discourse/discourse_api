437555cac9857a6a549d46176115d0966f6cbb07
Sam <sam.saffron@gmail.com>
2014-05-12 09:34:51 +1000

    extend sample

--- examples/invite_users.rb
+++ examples/invite_users.rb
@@ -8,3 +8,7 @@ client.api_key = "6157bad92b3b5f8004aedc82220281c5966e5d560bb2bb6f962123474db4ac
 client.api_username = "forumadmin"
 
 client.topic_invite_user(topic_id: 1, email: "bob@bob.com")
+
+# if the user is an admin you may invite to a group as well
+client.topic_invite_user(topic_id: 1, email: "bob@bob.com", group_ids: "1,2,3")
+client.topic_invite_user(topic_id: 1, email: "bob@bob.com", group_names: "bugs,cars,testers")
