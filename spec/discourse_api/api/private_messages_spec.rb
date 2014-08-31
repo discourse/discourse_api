35f5e2e165ed8ed748cd6256296327d214f06cb7
Blake Erickson <o.blakeerickson@gmail.com>
2014-08-31 07:35:33 -0600

    add private_messages

---
+++ spec/discourse_api/api/private_messages_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe DiscourseApi::API::PrivateMessages do
+  subject { DiscourseApi::Client.new("http://localhost") }
+
+  describe "#private_messages" do
+    it "needs to have a test written for it"
+  end
+
+end
