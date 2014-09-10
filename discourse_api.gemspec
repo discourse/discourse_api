ab30b01099f3b6fb8d3bd7cc26f649f1bff01728
Blake Erickson <o.blakeerickson@gmail.com>
2014-09-10 07:55:45 -0600

    set port to 3000 set version numbers on included gems got working with rspec 3.0.0 added api_key to urls allow localhost requests

--- discourse_api.gemspec
+++ discourse_api.gemspec
@@ -18,14 +18,14 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.add_dependency "faraday"
+  spec.add_dependency "faraday", "0.9.0"
   spec.add_dependency "faraday_middleware", "~> 0.9"
   spec.add_development_dependency "bundler", "~> 1.3"
-  spec.add_development_dependency "rake"
-  spec.add_development_dependency "rspec"
-  spec.add_development_dependency "webmock"
-  spec.add_development_dependency "guard-rspec"
-  spec.add_development_dependency "guard"
-  spec.add_development_dependency "rb-inotify"
-  spec.add_development_dependency "simplecov"
+  spec.add_development_dependency "rake", "10.3.2"
+  spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "webmock", "~> 1.18"
+  spec.add_development_dependency "guard-rspec", "~> 4.3"
+  spec.add_development_dependency "guard", "~> 2.6"
+  spec.add_development_dependency "rb-inotify", "~> 0.9"
+  spec.add_development_dependency "simplecov", "~> 0.9"
 end
--- spec/discourse_api/api/categories_spec.rb
+++ spec/discourse_api/api/categories_spec.rb
@@ -1,19 +1,23 @@
 require 'spec_helper'
 
 describe DiscourseApi::API::Categories do
-  subject { DiscourseApi::Client.new("http://localhost") }
+  subject { DiscourseApi::Client.new("http://localhost:3000") }
 
   describe "#categories" do
     before do
-      stub_get("http://localhost/categories.json").to_return(body: fixture("categories.json"), headers: { content_type: "application/json" })
+      stub_get("http://localhost:3000/categories.json?api_key=test_d7fd0429940&api_username=test_user").to_return(body: fixture("categories.json"), headers: { content_type: "application/json" })
     end
 
     it "requests the correct resource" do
+      subject.api_key = 'test_d7fd0429940'
+      subject.api_username = 'test_user'
       subject.categories
-      expect(a_get("http://localhost/categories.json")).to have_been_made
+      expect(a_get("http://localhost:3000/categories.json?api_key=test_d7fd0429940&api_username=test_user")).to have_been_made
     end
 
     it "returns the requested categories" do
+      subject.api_key = 'test_d7fd0429940'
+      subject.api_username = 'test_user'
       categories = subject.categories
       expect(categories).to be_an Array
       expect(categories.first).to be_a Hash
@@ -22,10 +26,12 @@ describe DiscourseApi::API::Categories do
 
   describe '#category_latest_topics' do
     before do
-      stub_get("http://localhost/category/category-slug.json?api_key&api_username").to_return(body: fixture("category_latest_topics.json"), headers: { content_type: "application/json" })
+      stub_get("http://localhost:3000/category/category-slug.json?api_key=test_d7fd0429940&api_username=test_user").to_return(body: fixture("category_latest_topics.json"), headers: { content_type: "application/json" })
     end
 
     it "returns the latest topics in a category" do
+      subject.api_key = 'test_d7fd0429940'
+      subject.api_username = 'test_user'
       latest_topics = subject.category_latest_topics('category-slug')
       expect(latest_topics).to be_an Array
     end
