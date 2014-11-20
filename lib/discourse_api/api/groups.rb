1b501a3c9566d26d7dd00c62d8fa3a4960957535
Jason W. May <jmay@pobox.com>
2014-11-20 12:22:40 -0800

    incremental membership changes for groups, with tests & example

--- lib/discourse_api/api/groups.rb
+++ lib/discourse_api/api/groups.rb
@@ -1,13 +1,21 @@
 module DiscourseApi
   module API
     module Groups
-      def create_group(name:, visible: false)
+      def create_group(name:, visible: true)
         post("/admin/groups", group: {name: name, visible: visible.to_s})
       end
 
       def groups
-        response = get("/admin/groups")
+        response = get("/admin/groups.json")
         response.body
+      end
+
+      def group_add(group_id, *usernames)
+        patch("/admin/groups/#{group_id}", changes: {add: usernames})
+      end
+
+      def group_remove(group_id, *usernames)
+        patch("/admin/groups/#{group_id}", changes: {delete: usernames})
       end
     end
   end
---
+++ spec/discourse_api/api/groups_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe DiscourseApi::API::Groups do
+  subject { DiscourseApi::Client.new("http://localhost:3000", "test_d7fd0429940", "test_user" )}
+
+  describe "#groups" do
+    before do
+      stub_get("http://localhost:3000/admin/groups.json?api_key=test_d7fd0429940&api_username=test_user").to_return(body: fixture("groups.json"), headers: { content_type: "application/json" })
+    end
+
+    it "requests the correct resource" do
+      subject.groups
+      expect(a_get("http://localhost:3000/admin/groups.json?api_key=test_d7fd0429940&api_username=test_user")).to have_been_made
+    end
+
+    it "returns the requested groups" do
+      groups = subject.groups
+      expect(groups).to be_an Array
+      groups.each { |g| expect(g).to be_a Hash }
+    end
+
+    it "create new groups" do
+      stub_post("http://localhost:3000/admin/groups?api_key=test_d7fd0429940&api_username=test_user")
+      subject.create_group(name: "test_group")
+      expect(a_post("http://localhost:3000/admin/groups?api_key=test_d7fd0429940&api_username=test_user").
+              with(body: {group: {name: "test_group", visible: "true"}})
+            ).to have_been_made
+    end
+
+    it "adds members" do
+      stub_request(:patch, "http://localhost:3000/admin/groups/123?api_key=test_d7fd0429940&api_username=test_user")
+      subject.group_add({id: 123}, "sam")
+      expect(a_request(:patch, "http://localhost:3000/admin/groups/123?api_key=test_d7fd0429940&api_username=test_user").
+              with(body: {changes: {add: [ "sam" ]}})
+            ).to have_been_made
+    end
+
+    it "removes members" do
+      stub_request(:patch, "http://localhost:3000/admin/groups/123?api_key=test_d7fd0429940&api_username=test_user")
+      subject.group_remove({id: 123}, "sam")
+      expect(a_request(:patch, "http://localhost:3000/admin/groups/123?api_key=test_d7fd0429940&api_username=test_user").
+              with(body: {changes: {delete: [ "sam" ]}})
+            ).to have_been_made
+    end
+  end
+end
