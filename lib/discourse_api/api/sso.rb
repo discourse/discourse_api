17e68e4365648abb9f69b7f95d9b0381daccea3d
Sam <sam.saffron@gmail.com>
2014-11-27 14:11:05 +1100

    Update Discourse SSO class and move to correct namespace

--- lib/discourse_api/api/sso.rb
+++ lib/discourse_api/api/sso.rb
@@ -1,10 +1,8 @@
-require 'single_sign_on'
-
 module DiscourseApi
   module API
     module SSO
       def sync_sso(params={})
-        sso = SingleSignOn.new
+        sso = DiscourseApi::SingleSignOn.new
         sso.sso_secret = params[:sso_secret]
         sso.name = params[:name]
         sso.username = params[:username]
