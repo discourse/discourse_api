deca15165d489ef828e8c5cc4a3630b5e316bfca
Jason W. May <jmay@pobox.com>
2014-12-04 16:53:42 -0800

    include optional params if provided but nil

--- lib/discourse_api/api/params.rb
+++ lib/discourse_api/api/params.rb
@@ -42,7 +42,7 @@ module DiscourseApi
             @args.dup
           else
             @optional.each do |k|
-              h[k] = @args[k] if @args[k]
+              h[k] = @args[k] if @args.include?(k)
             end
             h
           end
