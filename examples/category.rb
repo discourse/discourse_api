7d79eeac1a84ea4336d3270c3cac86f58e8faeb5
Arpit Jalan <arpit@techapj.com>
2014-12-12 21:10:13 +0530

    add example for fetching categories and sub-categories

---
+++ examples/category.rb
@@ -0,0 +1,12 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require File.expand_path('../../lib/discourse_api', __FILE__)
+
+client = DiscourseApi::Client.new("http://localhost:3000")
+client.api_key = "YOUR_API_KEY"
+client.api_username = "YOUR_USERNAME"
+
+# get categories
+puts client.categories()
+
+# get sub categories for parent category with id 2
+puts client.categories(parent_category_id: 2)
