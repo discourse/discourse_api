8ebfc9a030f48dfc3dbac297ec790bd3ee8476af
Blake Erickson <o.blakeerickson@gmail.com>
2015-01-13 22:00:25 -0700

    add ability to revoke api key

--- lib/discourse_api/api/api_admin.rb
+++ lib/discourse_api/api/api_admin.rb
@@ -9,6 +9,10 @@ module DiscourseApi
       def generate_api_key(user_id)
         response = post("/admin/users/#{user_id}/generate_api_key.json")
       end
+
+      def revoke_api_key(user_id)
+        response = delete("/admin/users/#{user_id}/revoke_api_key.json")
+      end
     end
   end
 end
