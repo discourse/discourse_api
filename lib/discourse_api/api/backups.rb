e73935fcd8b67a69f35bed375e6791225a220ed5
Arpit Jalan <arpit@techapj.com>
2015-04-08 22:49:53 +0530

    FEATURE: create and restore backup

--- lib/discourse_api/api/backups.rb
+++ lib/discourse_api/api/backups.rb
@@ -5,6 +5,14 @@ module DiscourseApi
         response = get("/admin/backups.json")
         response.body
       end
+
+      def create_backup
+        post("/admin/backups", with_uploads: true)
+      end
+
+      def restore_backup(file_name)
+        post("/admin/backups/#{file_name}/restore")
+      end
     end
   end
 end
