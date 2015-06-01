c065374d71e315125c0e87b54c41e7b213acead7
Arpit Jalan <arpit@techapj.com>
2015-06-01 19:05:08 +0530

    Add example for create_user endpoint

---
+++ examples/create_user.rb
@@ -0,0 +1,14 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require File.expand_path('../../lib/discourse_api', __FILE__)
+
+client = DiscourseApi::Client.new("http://localhost:3000")
+client.api_key = "YOUR_API_KEY"
+client.api_username = "YOUR_USERNAME"
+
+# create user
+client.create_user(
+  name: "Bruce Wayne",
+  email: "bruce@wayne.com",
+  username: "batman",
+  password: "WhySoSerious"
+)
