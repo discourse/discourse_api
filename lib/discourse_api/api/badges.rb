9aaff20ef75c8de01258f0a378b56324beada510
Neil Lalonde <neillalonde@gmail.com>
2015-06-29 15:31:52 -0400

    add create_badge

--- lib/discourse_api/api/badges.rb
+++ lib/discourse_api/api/badges.rb
@@ -14,6 +14,15 @@ module DiscourseApi
       def grant_user_badge(params={})
         post("/user_badges", params)
       end
+
+      def create_badge(params={})
+        args = API.params(params)
+                  .required(:name, :badge_type_id)
+                  .optional(:description, :allow_title, :multiple_grant, :icon, :listable,
+                            :target_posts, :query, :enabled, :auto_revoke, :badge_grouping_id,
+                            :trigger, :show_posts, :image, :long_description)
+        post("/admin/badges.json", args)
+      end
     end
   end
 end
