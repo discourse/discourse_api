28ba1b28ebcef79354215ffb85720acebb2ab6c7
Arpit Jalan <arpit@techapj.com>
2015-09-12 23:32:52 +0530

    add example for search

---
+++ examples/search.rb
@@ -0,0 +1,9 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require File.expand_path('../../lib/discourse_api', __FILE__)
+
+client = DiscourseApi::Client.new("http://localhost:3000")
+client.api_key = "YOUR_API_KEY"
+client.api_username = "YOUR_USERNAME"
+
+# search for term
+puts client.search("batman")
