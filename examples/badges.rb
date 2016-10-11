f251246ed8f8c4d04b3e3d603823deb9edaa855d
Rimian Perkins <hello@rimian.com.au>
2016-10-11 12:54:00 +1100

    example for badges

---
+++ examples/badges.rb
@@ -0,0 +1,28 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require File.expand_path('../../lib/discourse_api', __FILE__)
+
+client = DiscourseApi::Client.new("http://localhost:3000")
+client.api_key = "YOUR_API_KEY"
+client.api_username = "YOUR_USERNAME"
+
+# get badges
+puts client.badges
+
+# get badges for a user
+puts client.user_badges("test-user")
+
+# Grants a badge to a user.
+puts client.grant_user_badge(badge_id: 9, username: "test-user", reason: "Really nice person")
+
+# Creates a new badge
+
+###
+# Required params:
+#   :name, :badge_type_id (gold: 1, bronze: 3, silver: 2)
+# Optional params:
+#   :description, :allow_title, :multiple_grant, :icon, :listable,
+#   :target_posts, :query, :enabled, :auto_revoke, :badge_grouping_id,
+#   :trigger, :show_posts, :image, :long_description
+###
+
+puts client.create_badge(name: "Shiny new badge", badge_type_id: 1)
