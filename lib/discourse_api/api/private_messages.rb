92e8a75e472f23795aa9f9c7448f383402ed5530
Ali Ibrahim <aliibrahim@gmail.com>
2017-04-11 15:31:55 +0500

    Create private messages

--- lib/discourse_api/api/private_messages.rb
+++ lib/discourse_api/api/private_messages.rb
@@ -1,6 +1,18 @@
 module DiscourseApi
   module API
     module PrivateMessages
+
+      # :target_usernames REQUIRED comma separated list of usernames
+      # :category OPTIONAL name of category, not ID
+      # :created_at OPTIONAL seconds since epoch.
+      def create_private_message(args={})
+        args[:archetype] = 'private_message'
+        args = API.params(args)
+                  .required(:title, :raw, :target_usernames, :archetype)
+                  .optional(:category, :created_at, :api_username)
+        post("/posts", args.to_h)
+      end
+
       def private_messages(username, *args)
         response = get("topics/private-messages/#{username}.json", args)
         response[:body]['topic_list']['topics']
