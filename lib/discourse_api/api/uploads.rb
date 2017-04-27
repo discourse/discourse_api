e82d988f1bf2c3fd4072d0ebdf0e11da64406918
Ali Ibrahim <aliibrahim@gmail.com>
2017-04-27 17:18:56 +0500

    Added endpoint to upload file

--- lib/discourse_api/api/uploads.rb
+++ lib/discourse_api/api/uploads.rb
@@ -8,6 +8,14 @@ module DiscourseApi
                   .to_h
         post('/uploads', args)
       end
+
+      def upload_file(args)
+        args = API.params(args)
+                  .optional(:file, :url)
+                  .default(type: 'upload', synchronous: true)
+                  .to_h
+        post('/uploads', args)
+      end
     end
   end
 end
