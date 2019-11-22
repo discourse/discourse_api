bce93f6da7c8c61a242e744cac546d02691d7ae7
Kim Miller <kimardenmiller@gmail.com>
2019-11-22 11:28:40 -0800

    - Example files now read config.yml file when present for client settings (#189)

--- examples/post_action.rb
+++ examples/post_action.rb
@@ -2,9 +2,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require File.expand_path('../../lib/discourse_api', __FILE__)
 
-client = DiscourseApi::Client.new("http://localhost:3000")
-client.api_key = "YOUR_API_KEY"
-client.api_username = "YOUR_USERNAME"
+config = DiscourseApi::ExampleHelper.load_yml
+
+client = DiscourseApi::Client.new(config['host'] || 'http://localhost:3000')
+client.api_key = config['api_key'] || "YOUR_API_KEY"
+client.api_username = config['api_username'] || "YOUR_USERNAME"
 
 # Post Action Type IDs
 # 1 - Bookmark
