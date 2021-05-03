3c5d53e54e23ff1b9013d1be9a743797f060b4dd
Ahmed <ahmedgagan6@gmail.com>
2021-05-04 03:16:34 +0530

    DEV: seperates required & optional params in invite module

--- lib/discourse_api/api/invite.rb
+++ lib/discourse_api/api/invite.rb
@@ -3,10 +3,30 @@ module DiscourseApi
   module API
     module Invite
       def invite_user(params = {})
+        args = API.params(params)
+          .optional(
+            :email,
+            :skip_email,
+            :custom_message,
+            :max_redemptions_allowed,
+            :topic_id,
+            :group_ids,
+            :expires_at
+          ).to_h
+
         post("/invites", params)
       end
 
       def invite_user_to_topic(params = {})
+        args = API.params(params)
+          .required(:topic_id)
+          .optional(
+            :email,
+            :user,
+            :group_ids,
+            :custom_message
+          ).to_h
+
         post("/t/#{params[:topic_id]}/invite", params)
       end
 
