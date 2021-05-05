ef984bbcf4b081d1b3f44c83e7243a33183d382a
Ahmed <ahmedgagan6@gmail.com>
2021-05-06 00:41:05 +0530

    DEV: deprecated 'invite_user_to_topic' method

--- examples/invite_users.rb
+++ examples/invite_users.rb
@@ -12,7 +12,7 @@ client.api_username = config['api_username'] || "YOUR_USERNAME"
 client.invite_user(email: "name@example.com", group_ids: "41,42")
 
 # invite to a topic
-client.invite_user_to_topic(1, email: "foo@bar.com")
+client.invite_user_to_topic(email: "foo@bar.com", topic_id: 1)
 
 # if the user is an admin you may invite to a group as well
-client.invite_user_to_topic(1, email: "foo@bar.com", group_ids: "1,2,3")
+client.invite_user_to_topic(email: "foo@bar.com", group_ids: "1,2,3", topic_id: 1)
